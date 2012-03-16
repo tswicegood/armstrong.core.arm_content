cc016e15eb3f6812e25df2a8f94724d8cf72f994
Travis Swicegood <development@domain51.com>
2012-03-16 11:21:30 -0500

    Add docblock

--- armstrong/core/arm_content/views/publication.py
+++ armstrong/core/arm_content/views/publication.py
@@ -4,6 +4,14 @@ from django.views.generic.detail import DetailView
 
 class PublishedModelDetailView(DetailView):
     def get_queryset(self):
+        """
+        Returns queryset filtered for only published models
+
+        This makes the assumption that the provided ``model`` attribute
+        has a ``published`` property that works as a queryset.  If present,
+        it will call ``select_subclasses``.  The ``PublishedManager`` that
+        Armstrong provides has a ``select_subclasses`` method.
+        """
         try:
             qs = self.model.published.all()
             try:
