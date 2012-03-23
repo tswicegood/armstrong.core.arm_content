d82351fb5f6b1c8371f11b287546fba9617329cb
Travis Swicegood <development@domain51.com>
2012-03-23 16:14:53 -0500

    Moves the PublishedManager to the inside of the publication module

--- armstrong/core/arm_content/managers/publication.py
+++ armstrong/core/arm_content/managers/publication.py
@@ -1,11 +1,1 @@
-from datetime import datetime
-from model_utils.managers import InheritanceManager
-
-from ..publication.utils import add_publication_filters
-
-
-class PublishedManager(InheritanceManager):
-    """ Returns published objects where the pub_date has already passed """
-    def get_query_set(self):
-        qs = super(PublishedManager, self).get_query_set()
-        return add_publication_filters(qs)
+from ..publication.managers import PublishedManager
